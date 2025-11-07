752e0e06c545fe9f4e30bdf8e2ee255f3b213950
Emanuel Pereira <emanuelcdpr@gmail.com.br>
2025-11-07 18:59:20 -0300

    feat: configure global css and font-family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Roboto } from 'next/font/google';
 
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
+  style: ['normal', 'italic'],
+  variable: '--font-roboto', 
+});
 
 export const metadata: Metadata = {
   title: "Money-app",
@@ -12,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={roboto.className}>
       <body>
         {children}
       </body>
